1507f62d00ed9b6376b3af7590f02bb469eb0b65
Jaebins <jen2065@naver.com>
2022-09-18 12:56:07 +0900

    1차

---
+++ SchoolKiosk/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿using System.Windows.Forms;
+
+namespace Kiosk
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input_num0 = new System.Windows.Forms.TextBox();
+            this.input_num1 = new System.Windows.Forms.TextBox();
+            this.input_num2 = new System.Windows.Forms.TextBox();
+            this.input_num3 = new System.Windows.Forms.TextBox();
+            this.input_num4 = new System.Windows.Forms.TextBox();
+            this.panel_NumsInputBack = new System.Windows.Forms.Panel();
+            this.but_1 = new System.Windows.Forms.Button();
+            this.but_2 = new System.Windows.Forms.Button();
+            this.but_3 = new System.Windows.Forms.Button();
+            this.but_6 = new System.Windows.Forms.Button();
+            this.but_5 = new System.Windows.Forms.Button();
+            this.but_4 = new System.Windows.Forms.Button();
+            this.but_9 = new System.Windows.Forms.Button();
+            this.but_8 = new System.Windows.Forms.Button();
+            this.but_7 = new System.Windows.Forms.Button();
+            this.but_Suc = new System.Windows.Forms.Button();
+            this.but_0 = new System.Windows.Forms.Button();
+            this.but_Back = new System.Windows.Forms.Button();
+            this.panel_Screen2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_timerCount = new System.Windows.Forms.Label();
+            this.label_SuccesText = new System.Windows.Forms.Label();
+            this.panel_Screen1 = new System.Windows.Forms.Panel();
+            this.panel_NumsInputBack.SuspendLayout();
+            this.panel_Screen2.SuspendLayout();
+            this.panel_Screen1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // input_num0
+            // 
+            this.input_num0.Font = new System.Drawing.Font("Sitka Subheading", 21.75F, System.Drawing.FontStyle.Bold);
+            this.input_num0.Location = new System.Drawing.Point(84, 57);
+            this.input_num0.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.input_num0.Name = "input_num0";
+            this.input_num0.Size = new System.Drawing.Size(52, 44);
+            this.input_num0.TabIndex = 5;
+            // 
+            // input_num1
+            // 
+            this.input_num1.Font = new System.Drawing.Font("Sitka Subheading", 21.75F, System.Drawing.FontStyle.Bold);
+            this.input_num1.Location = new System.Drawing.Point(142, 57);
+            this.input_num1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.input_num1.Name = "input_num1";
+            this.input_num1.Size = new System.Drawing.Size(52, 44);
+            this.input_num1.TabIndex = 1;
+            // 
+            // input_num2
+            // 
+            this.input_num2.Font = new System.Drawing.Font("Sitka Subheading", 21.75F, System.Drawing.FontStyle.Bold);
+            this.input_num2.Location = new System.Drawing.Point(200, 57);
+            this.input_num2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.input_num2.Name = "input_num2";
+            this.input_num2.Size = new System.Drawing.Size(52, 44);
+            this.input_num2.TabIndex = 2;
+            // 
+            // input_num3
+            // 
+            this.input_num3.Font = new System.Drawing.Font("Sitka Subheading", 21.75F, System.Drawing.FontStyle.Bold);
+            this.input_num3.Location = new System.Drawing.Point(258, 57);
+            this.input_num3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.input_num3.Name = "input_num3";
+            this.input_num3.Size = new System.Drawing.Size(52, 44);
+            this.input_num3.TabIndex = 3;
+            // 
+            // input_num4
+            // 
+            this.input_num4.Font = new System.Drawing.Font("Sitka Subheading", 21.75F, System.Drawing.FontStyle.Bold);
+            this.input_num4.Location = new System.Drawing.Point(316, 57);
+            this.input_num4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.input_num4.Name = "input_num4";
+            this.input_num4.Size = new System.Drawing.Size(52, 44);
+            this.input_num4.TabIndex = 4;
+            // 
+            // panel_NumsInputBack
+            // 
+            this.panel_NumsInputBack.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel_NumsInputBack.Controls.Add(this.input_num4);
+            this.panel_NumsInputBack.Controls.Add(this.input_num3);
+            this.panel_NumsInputBack.Controls.Add(this.input_num0);
+            this.panel_NumsInputBack.Controls.Add(this.input_num2);
+            this.panel_NumsInputBack.Controls.Add(this.input_num1);
+            this.panel_NumsInputBack.Location = new System.Drawing.Point(0, 0);
+            this.panel_NumsInputBack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel_NumsInputBack.Name = "panel_NumsInputBack";
+            this.panel_NumsInputBack.Size = new System.Drawing.Size(450, 160);
+            this.panel_NumsInputBack.TabIndex = 0;
+            this.panel_NumsInputBack.TabStop = true;
+            // 
+            // but_1
+            // 
+            this.but_1.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold);
+            this.but_1.Location = new System.Drawing.Point(0, 160);
+            this.but_1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_1.Name = "but_1";
+            this.but_1.Size = new System.Drawing.Size(150, 80);
+            this.but_1.TabIndex = 1;
+            this.but_1.Text = "1";
+            this.but_1.UseVisualStyleBackColor = true;
+            // 
+            // but_2
+            // 
+            this.but_2.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold);
+            this.but_2.Location = new System.Drawing.Point(150, 160);
+            this.but_2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_2.Name = "but_2";
+            this.but_2.Size = new System.Drawing.Size(150, 80);
+            this.but_2.TabIndex = 3;
+            this.but_2.Text = "2";
+            this.but_2.UseVisualStyleBackColor = true;
+            // 
+            // but_3
+            // 
+            this.but_3.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold);
+            this.but_3.Location = new System.Drawing.Point(300, 160);
+            this.but_3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_3.Name = "but_3";
+            this.but_3.Size = new System.Drawing.Size(150, 80);
+            this.but_3.TabIndex = 4;
+            this.but_3.Text = "3";
+            this.but_3.UseVisualStyleBackColor = true;
+            // 
+            // but_6
+            // 
+            this.but_6.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold);
+            this.but_6.Location = new System.Drawing.Point(300, 240);
+            this.but_6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_6.Name = "but_6";
+            this.but_6.Size = new System.Drawing.Size(150, 80);
+            this.but_6.TabIndex = 7;
+            this.but_6.Text = "6";
+            this.but_6.UseVisualStyleBackColor = true;
+            // 
+            // but_5
+            // 
+            this.but_5.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold);
+            this.but_5.Location = new System.Drawing.Point(150, 240);
+            this.but_5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_5.Name = "but_5";
+            this.but_5.Size = new System.Drawing.Size(150, 80);
+            this.but_5.TabIndex = 6;
+            this.but_5.Text = "5";
+            this.but_5.UseVisualStyleBackColor = true;
+            // 
+            // but_4
+            // 
+            this.but_4.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold);
+            this.but_4.Location = new System.Drawing.Point(0, 240);
+            this.but_4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_4.Name = "but_4";
+            this.but_4.Size = new System.Drawing.Size(150, 80);
+            this.but_4.TabIndex = 5;
+            this.but_4.Text = "4";
+            this.but_4.UseVisualStyleBackColor = true;
+            // 
+            // but_9
+            // 
+            this.but_9.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold);
+            this.but_9.Location = new System.Drawing.Point(300, 320);
+            this.but_9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_9.Name = "but_9";
+            this.but_9.Size = new System.Drawing.Size(150, 80);
+            this.but_9.TabIndex = 10;
+            this.but_9.Text = "9";
+            this.but_9.UseVisualStyleBackColor = true;
+            // 
+            // but_8
+            // 
+            this.but_8.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold);
+            this.but_8.Location = new System.Drawing.Point(150, 320);
+            this.but_8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_8.Name = "but_8";
+            this.but_8.Size = new System.Drawing.Size(150, 80);
+            this.but_8.TabIndex = 9;
+            this.but_8.Text = "8";
+            this.but_8.UseVisualStyleBackColor = true;
+            // 
+            // but_7
+            // 
+            this.but_7.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold);
+            this.but_7.Location = new System.Drawing.Point(0, 320);
+            this.but_7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_7.Name = "but_7";
+            this.but_7.Size = new System.Drawing.Size(150, 80);
+            this.but_7.TabIndex = 8;
+            this.but_7.Text = "7";
+            this.but_7.UseVisualStyleBackColor = true;
+            // 
+            // but_Suc
+            // 
+            this.but_Suc.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold);
+            this.but_Suc.Location = new System.Drawing.Point(300, 400);
+            this.but_Suc.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_Suc.Name = "but_Suc";
+            this.but_Suc.Size = new System.Drawing.Size(150, 80);
+            this.but_Suc.TabIndex = 13;
+            this.but_Suc.Text = "Suc";
+            this.but_Suc.UseVisualStyleBackColor = true;
+            // 
+            // but_0
+            // 
+            this.but_0.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold);
+            this.but_0.Location = new System.Drawing.Point(150, 400);
+            this.but_0.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_0.Name = "but_0";
+            this.but_0.Size = new System.Drawing.Size(150, 80);
+            this.but_0.TabIndex = 12;
+            this.but_0.Text = "0";
+            this.but_0.UseVisualStyleBackColor = true;
+            // 
+            // but_Back
+            // 
+            this.but_Back.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold);
+            this.but_Back.Location = new System.Drawing.Point(0, 400);
+            this.but_Back.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.but_Back.Name = "but_Back";
+            this.but_Back.Size = new System.Drawing.Size(150, 80);
+            this.but_Back.TabIndex = 11;
+            this.but_Back.Text = "<";
+            this.but_Back.UseVisualStyleBackColor = true;
+            // 
+            // panel_Screen2
+            // 
+            this.panel_Screen2.Controls.Add(this.label1);
+            this.panel_Screen2.Controls.Add(this.label_timerCount);
+            this.panel_Screen2.Controls.Add(this.label_SuccesText);
+            this.panel_Screen2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_Screen2.Location = new System.Drawing.Point(0, 0);
+            this.panel_Screen2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel_Screen2.Name = "panel_Screen2";
+            this.panel_Screen2.Size = new System.Drawing.Size(450, 480);
+            this.panel_Screen2.TabIndex = 14;
+            this.panel_Screen2.Visible = false;
+            this.panel_Screen2.Click += new System.EventHandler(this.panel_Screen2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(138, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "화면을 클릭해주세요.";
+            // 
+            // label_timerCount
+            // 
+            this.label_timerCount.AutoSize = true;
+            this.label_timerCount.Font = new System.Drawing.Font("맑은 고딕", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label_timerCount.Location = new System.Drawing.Point(206, 291);
+            this.label_timerCount.Name = "label_timerCount";
+            this.label_timerCount.Size = new System.Drawing.Size(33, 40);
+            this.label_timerCount.TabIndex = 1;
+            this.label_timerCount.Text = "3";
+            // 
+            // label_SuccesText
+            // 
+            this.label_SuccesText.AutoSize = true;
+            this.label_SuccesText.Font = new System.Drawing.Font("맑은 고딕", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label_SuccesText.Location = new System.Drawing.Point(27, 93);
+            this.label_SuccesText.Name = "label_SuccesText";
+            this.label_SuccesText.Size = new System.Drawing.Size(391, 47);
+            this.label_SuccesText.TabIndex = 0;
+            this.label_SuccesText.Text = "출석을 완료하였습니다.";
+            // 
+            // panel_Screen1
+            // 
+            this.panel_Screen1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel_Screen1.Controls.Add(this.but_Suc);
+            this.panel_Screen1.Controls.Add(this.but_0);
+            this.panel_Screen1.Controls.Add(this.but_Back);
+            this.panel_Screen1.Controls.Add(this.but_9);
+            this.panel_Screen1.Controls.Add(this.but_8);
+            this.panel_Screen1.Controls.Add(this.but_7);
+            this.panel_Screen1.Controls.Add(this.but_6);
+            this.panel_Screen1.Controls.Add(this.but_5);
+            this.panel_Screen1.Controls.Add(this.but_4);
+            this.panel_Screen1.Controls.Add(this.but_3);
+            this.panel_Screen1.Controls.Add(this.but_2);
+            this.panel_Screen1.Controls.Add(this.but_1);
+            this.panel_Screen1.Controls.Add(this.panel_NumsInputBack);
+            this.panel_Screen1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_Screen1.Location = new System.Drawing.Point(0, 0);
+            this.panel_Screen1.Name = "panel_Screen1";
+            this.panel_Screen1.Size = new System.Drawing.Size(450, 480);
+            this.panel_Screen1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 480);
+            this.Controls.Add(this.panel_Screen2);
+            this.Controls.Add(this.panel_Screen1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel_NumsInputBack.ResumeLayout(false);
+            this.panel_NumsInputBack.PerformLayout();
+            this.panel_Screen2.ResumeLayout(false);
+            this.panel_Screen2.PerformLayout();
+            this.panel_Screen1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TextBox input_num0;
+        private TextBox input_num1;
+        private TextBox input_num2;
+        private TextBox input_num3;
+        private TextBox input_num4;
+        private Panel panel_NumsInputBack;
+        private Button but_1;
+        private Button but_2;
+        private Button but_3;
+        private Button but_6;
+        private Button but_5;
+        private Button but_4;
+        private Button but_9;
+        private Button but_8;
+        private Button but_7;
+        private Button but_Suc;
+        private Button but_0;
+        private Button but_Back;
+        private Panel panel_Screen2;
+        private Label label_SuccesText;
+        private Label label_timerCount;
+        private Panel panel_Screen1;
+        private Label label1;
+    }
+}
---
+++ SchoolKiosk/Form1.cs
@@ -0,0 +1,129 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Windows.Forms;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Kiosk
+{
+    public partial class Form1 : Form
+    {
+        TextBox[] input_Nums = new TextBox[5];
+        string resultInputNums = string.Empty;
+
+        List<string> students = new List<string>();
+        int timerCount = 0;
+        Timer screenTurnTimer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string studentNums = "{ " +
+                    "\"test\": [ " +
+                        "{ \"num\" : \"21101\" }, " +
+                        "{ \"num\" : \"21102\" }, " +
+                        "{ \"num\": \"21103\" }, " +
+                        "{ \"num\": \"21104\" }, " +
+                    "] " +
+                "}";
+            JObject jObject = JObject.Parse(studentNums);
+            JToken jToken = jObject["test"];
+            foreach (JToken members in jToken)
+            {
+                students.Add(members["num"].ToString());
+            }
+
+            input_Nums[0] = input_num0;
+            input_Nums[1] = input_num1;
+            input_Nums[2] = input_num2;
+            input_Nums[3] = input_num3;
+            input_Nums[4] = input_num4;
+
+            this.but_1.Click += (sender2, e2) => { but_Click(sender2, e2, 1); };
+            this.but_2.Click += (sender2, e2) => { but_Click(sender2, e2, 2); };
+            this.but_3.Click += (sender2, e2) => { but_Click(sender2, e2, 3); };
+            this.but_4.Click += (sender2, e2) => { but_Click(sender2, e2, 4); };
+            this.but_5.Click += (sender2, e2) => { but_Click(sender2, e2, 5); };
+            this.but_6.Click += (sender2, e2) => { but_Click(sender2, e2, 6); };
+            this.but_7.Click += (sender2, e2) => { but_Click(sender2, e2, 7); };
+            this.but_8.Click += (sender2, e2) => { but_Click(sender2, e2, 8); };
+            this.but_9.Click += (sender2, e2) => { but_Click(sender2, e2, 9); };
+            this.but_0.Click += (sender2, e2) => { but_Click(sender2, e2, 0); };
+            this.but_Suc.Click += (sender2, e2) => { but_Click(sender2, e2, 10); };
+            this.but_Back.Click += (sender2, e2) => { but_Click(sender2, e2, -1); };
+        }
+
+        private void but_Click(object sender, EventArgs e, int num)
+        {
+            if (num != 10 && num != -1 && resultInputNums.Length < 5) // 완료
+            {
+                resultInputNums += num.ToString();
+                input_Nums[resultInputNums.Length - 1].Text = resultInputNums[resultInputNums.Length - 1].ToString();
+            }
+            else if (num == -1 && resultInputNums.Length > 0) // 지우기
+            {
+                resultInputNums = resultInputNums.Substring(0, resultInputNums.Length - 1);
+                input_Nums[resultInputNums.Length].Text = "";
+            }
+            else if (num == 10 && resultInputNums.Length < 5)
+            {
+                MessageBox.Show("입력되지 않았습니다.");
+            }
+            else if (num == 10)
+            {
+                CheckClass();
+            }
+        }
+
+        void CheckClass()
+        {
+            if (students.Contains(resultInputNums))
+            {
+                panel_Screen2.Visible = true;
+                label_timerCount.Text = (3 - timerCount).ToString();
+                screenTurnTimer.Interval = 1000;
+                screenTurnTimer.Tick += (sender, e) => timer_GoInitScene(sender, e);
+                screenTurnTimer.Start();
+            }
+            else
+            {
+                MessageBox.Show("올바르지 않은 번호입니다.");
+            }
+        }
+
+        void timer_GoInitScene(object sender, EventArgs e)
+        {
+            label_timerCount.Text = (3 - timerCount).ToString();
+            if(timerCount < 3)
+            {
+                timerCount++;
+            }
+            else
+            {
+                ResetSetting();
+            }
+        }
+
+        private void panel_Screen2_Click(object sender, EventArgs e)
+        {
+            ResetSetting();
+        }
+
+        void ResetSetting()
+        {
+            screenTurnTimer.Stop();
+            screenTurnTimer = new Timer();
+            panel_Screen2.Visible = false;
+            timerCount = 0;
+            for (int i = 0; i < input_Nums.Length; i++)
+            {
+                input_Nums[i].Text = "";
+            }
+            resultInputNums = "";
+        }
+    }
+}
